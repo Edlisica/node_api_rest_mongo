ca0791a4d6bad293d884d5a04ef0e6d7d086f93d
Ed <enmanueljiron@outlook.com>
2025-01-14 22:25:57 -0600

    Le aplique formato a book.routes.js

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -72,88 +72,79 @@ router.post("/", async (req, res) => {
   }
 });
 
-
 //Get para id especifico
 
-router.get('/:id', getBook, async(req, res) => {
-    res.json(res.book);
-})
-
+router.get("/:id", getBook, async (req, res) => {
+  res.json(res.book);
+});
 
 // Put
 
-
-router.put('/:id', getBook, async (req, res) => {
-   try {
-    const book = res.book
+router.put("/:id", getBook, async (req, res) => {
+  try {
+    const book = res.book;
     book.title = req.body.title || book.title;
     book.author = req.body.author || book.author;
     book.genre = req.body.genre || book.genre;
-    book.publication_date = req.body.publication_date || book.body.publication_date;
+    book.publication_date =
+      req.body.publication_date || book.body.publication_date;
 
-    const updatedBook = await book.save()
-    res.json(updatedBook)
-
-   } catch (error) {
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } catch (error) {
     res.status(400).json({
-        message: error.message
-    })
-   }
-
-})
-
+      message: error.message,
+    });
+  }
+});
 
 // Patch
 
-router.patch('/:id', getBook, async (req, res) => {
+router.patch("/:id", getBook, async (req, res) => {
+  if (
+    !req.body.title &&
+    !req.body.author &&
+    !req.body.genre &&
+    !req.body.publication_date
+  ) {
+    res.status(400).json({
+      message:
+        "Al menos uno de estos campos debe ser enviado: Titulo, autor, genero o fecha de publicacion",
+    });
+  }
 
-    if(!req.body.title && !req.body.author && !req.body.genre && !req.body.publication_date){
-        res.status(400).json({
-            message: 'Al menos uno de estos campos debe ser enviado: Titulo, autor, genero o fecha de publicacion'
-        })
-    }
+  try {
+    const book = res.book;
+    book.title = req.body.title || book.title;
+    book.author = req.body.author || book.author;
+    book.genre = req.body.genre || book.genre;
+    book.publication_date = req.body.publication_date || book.publication_date;
 
-
-    try {
-        const book = res.book
-        book.title = req.body.title || book.title;
-        book.author = req.body.author || book.author;
-        book.genre = req.body.genre || book.genre;
-        book.publication_date = req.body.publication_date || book.publication_date;
- 
-     const updatedBook = await book.save()
-     res.json(updatedBook)
- 
-    } catch (error) {
-     res.status(400).json({
-         message: error.message
-     })
-    }
- 
- })
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
 
 // Delete
 
-router.delete('/:id', getBook, async(req, res) => {
+router.delete("/:id", getBook, async (req, res) => {
   try {
-    const book = res.book
+    const book = res.book;
     await book.deleteOne({
-      _id: book._id
+      _id: book._id,
     });
     res.json({
-      message: `El libro ${book.title} fue eliminado correctamente`
-    })
-
+      message: `El libro ${book.title} fue eliminado correctamente`,
+    });
   } catch (error) {
     res.status(500).json({
-      message: error.message
-    })
+      message: error.message,
+    });
   }
-})
+});
 
-
-
-
-
-
-module.exports = router
+module.exports = router;
