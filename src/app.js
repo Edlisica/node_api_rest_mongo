83b1f880717268c0e8b5714c8b996d05b32f2ff5
Ed <enmanueljiron@outlook.com>
2025-01-16 15:18:40 -0600

    quite mb compass para poner mongdb atalas

--- src/app.js
+++ src/app.js
@@ -12,8 +12,22 @@ const app = express();
 app.use(bodyParser.json()) // Parseador de Bodies
 
 // Acá conectaremos la base de datos:
-mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
-const db = mongoose.connection;
+// mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME })
+// const db = mongoose.connection;
+
+
+
+// Conectar a MongoDB Atlas
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.MONGO_DB_NAME
+})
+.then(() => console.log("🔥 Conectado a MongoDB Atlas"))
+.catch(err => console.error("❌ Error al conectar a MongoDB:", err));
+
+
+
 
 
 
@@ -24,3 +38,5 @@ const port = process.env.PORT || 3000
 app.listen(port, ()=> {
     console.log(`Servidor iniciado en el puerto ${port}`)
 })
+
+
